0846ea8a32cd7860b4406c39bda1929bc893ff26
Kartik Sundararajan <kartiksundarajan@gmail.com>
2016-06-18 23:12:00 -0700

    Fix tests

--- tests/test_toggl.py
+++ tests/test_toggl.py
@@ -1,11 +1,14 @@
 """
 Test toggl.py
 """
+import json
+
+import pytest
 
 from utils import toggl
 
 
-class RequestOutput(object):
+class RequestGet(object):
     def __init__(self):
         self.status_code = 200
 
@@ -49,9 +52,23 @@ class RequestOutput(object):
             u'id': 400164008}]
 
 
+class RequestPut(object):
+    def __init__(self, data):
+        self.data = data
+        self.status_code = 200
+
+    def json(self):
+        """Decode json data"""
+        return json.loads(self.data)['time_entry']
+
+def test_initialisation_with_incorrect_key():
+    with pytest.raises(KeyError) as error:
+        toggl.Connect('invalid_key')
+
+
 def test_get_time_logs(monkeypatch):
-    """Test timelog query"""
-    monkeypatch.setattr('requests.get', lambda x, params, auth: RequestOutput())
+    """Test time log query"""
+    monkeypatch.setattr('requests.get', lambda x, params, auth: RequestGet())
 
     connect = toggl.Connect()
     # Test if marked fields are ignored
@@ -63,3 +80,26 @@ def test_get_time_logs(monkeypatch):
 
     # Test for filtered data with ignore_marked True
     assert connect.get_time_logs(ignore_marked=True)[0]['id'] == 400163568
+
+
+def test_update(monkeypatch):
+    """Test update functionality while marking entries"""
+    monkeypatch.setattr('requests.put',
+                        lambda url, auth, data: RequestPut(data))
+
+    connect = toggl.Connect()
+    connect.tag_as_read = 'test_tag'
+    data = {
+        u'duronly': False,
+        u'wid': 140891,
+        u'description': u'test project',
+        u'stop': u'2016-06-16T18:40:49+00:00',
+        u'start': u'2016-06-16T18:09:00+00:00',
+        u'at': u'2016-06-19T04:36:33+00:00',
+        u'billable': False,
+        u'tags': ['my_tag'],
+        u'duration': 1909,
+        u'guid': u'2835f522-f475-44db-8071-1d067a3e6c88',
+        u'id': 400163419,
+        u'uid': 193045}
+    assert connect.mark_as_read(data).json()['tags'] == ['my_tag', 'test_tag']
